aea46d63aba3fe523abcfd4097918ad9f43e8ee8
Matthew Eldredge <mattheweldredge@gmail.com>
2017-12-01 19:49:35 -0700

    Palindrome.js component

--- src/components/Topics/Palindrome.js
+++ src/components/Topics/Palindrome.js
@@ -1,9 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 export default class Palindrome extends Component {
+
+    constructor() {
+        super();
+
+        this.state = {
+            userInput: '',
+            palindrome: ''
+        };
+    }
+
+    handleChange(val) {
+        this.setState({ userInput: val });
+    }
+
+    isPalindrome(userInput) {
+        var forward = userInput;
+        var backward = userInput;
+        backward = backward.split('').reverse().join('');
+        
+        forward === backward ? this.setState({ palindrome: 'true' }) : this.setState({ palindrome: 'false' });
+    }
+
     render() {
         return (
-            <p>Palindrome Component</p>
+            <div className="puzzleBox palindromePB">
+                <h4> Palindrome </h4>
+                <input className="inputLine" onChange={(e) => this.handleChange(e.target.value)}></input>
+                <button className="confirmationButton" onClick={() => this.isPalindrome(this.state.userInput)}> Check </button>
+                <span className="resultsBox"> Palindrome: {this.state.palindrome} </span>
+            </div>
         )
     }
 }
